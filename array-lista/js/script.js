39e18c6bc125c7098b2786d45295616dbeade0ca
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2023-12-23 00:08:14 +0100

    added schedule of second exercise

---
+++ array-lista/js/script.js
@@ -0,0 +1,9 @@
+console.log('JS OK')
+
+// ---------- La coda dell'Array ----------
+
+// 1 - costruisco la domanda assegnandogli una variabile
+// 2 - assegno all'array una variabile 
+// 3 - creo un ciclo for e aggiungo all'array un numero random per il numero di volte dichiarato dall'utente
+// 4 - stampo in console il risutlato in console
+
