e7c56c2cb38a44c9b84bcc50a2bf454caf77beab
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2023-12-22 20:33:11 +0100

    added schedule of first exercise

---
+++ array_tail/js/script.js
@@ -0,0 +1,10 @@
+console.log('JS OK')
+
+// ---------- TuttiFrutti ----------
+
+// 1 - dichiaro l'array del frigo con i frutti dentro
+// 2 - aggiungo la stringa della pesca all'erray
+// 3 - creo un ciclo for e aggiungo la pesca all'erray
+// 4 - innesto una condizionale dentro al ciclo for per verificare se il cocomero è all'interno dell'erray
+// 5 - stampo in console il risutlato
+
