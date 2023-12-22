f2e61db2df1b247b9afaabe19ff5ed3f5b2e4c97
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2023-12-23 00:09:14 +0100

    completed second exercise

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -7,3 +7,27 @@ console.log('JS OK')
 // 3 - creo un ciclo for e aggiungo all'array un numero random per il numero di volte dichiarato dall'utente
 // 4 - stampo in console il risutlato in console
 
+let number = parseInt(prompt("Quanti elementi vuoi che contenga l'Array?"))
+
+let ciccio = []
+
+console.log(number)
+
+for (let i = 1; i <= number; i++ ) {
+    ciccio.push(Math.floor(Math.random() * 100) + 1);
+
+}
+
+console.log(ciccio)
+
+
+console.log(ciccio[number - 1])
+
+console.log(ciccio[number - 2])
+
+console.log(ciccio[number - 3])
+
+console.log(ciccio[number - 4])
+
+console.log(ciccio[number - 5])
+
