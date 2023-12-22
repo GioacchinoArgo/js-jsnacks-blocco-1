32eecf8c43bb7d29541aa66217df0ba5b4080216
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2023-12-22 20:34:33 +0100

    completed first exercise

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -8,3 +8,25 @@ console.log('JS OK')
 // 4 - innesto una condizionale dentro al ciclo for per verificare se il cocomero è all'interno dell'erray
 // 5 - stampo in console il risutlato
 
+let fruits = ['banana','mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
+
+console.log(fruits);
+
+fruits.push('pesca');
+console.log(fruits);
+
+let watermelon = false
+
+for ( let i = 0; i < fruits.length; i++) {
+   console.log(fruits[i])
+    
+   if (fruits[i] === 'cocomero') {
+    watermelon = true
+    console.log('Trovato! Devo solo preparare il cocktail.')
+   }
+}
+
+if (watermelon) console.log('Trovato! Devo solo preparare il cocktail.')
+else  console.log("Oh no, devo uscire a comprare il cocomero!")
+
+
